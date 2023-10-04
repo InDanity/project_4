e26f3b8fec976a56ad2910fdcc517a8aa138fe3f
Nandita <74115831+ta0202@users.noreply.github.com>
2023-10-04 23:40:51 +0000

    got it somewhat running; added 1 node (the first, maybe incorrectly??)
    run this in directory terminal for 1 step debugging for now i think
    
    make; ./scheduler RR jobFile.txt 0

--- README.txt
+++ README.txt
@@ -11,7 +11,47 @@ Passing all tests are not sufficient to obtain all points for this assignment. A
 10/04/23
 
 Making the basic layout of the project:
-    Using int main(int argc, char *argv[]) to get all arguments.
+	- Using int main(int argc, char *argv[]) to get all arguments.
 
+Made Makefile, tried to get program to run w/o full functionality
+	- Kept running into segmentation fault
+	- Tried to debug using the coredump file like in project0
+		- This said issue was with "feof":
+			Program terminated with signal SIGSEGV, Segmentation fault.
+			#0  _IO_feof (fp=0x0) at ./libio/feof.c:35
+			35      ./libio/feof.c: No such file or directory.
+		- Maybe we are not including the right libraries??
+		- No its def in <stdio.h> according to the man pages...
+	- Uh so tried to comment out the whole feof while loop and still and running into seg fault??
+	- I'm gonna guess it's an issue with the command line args which we should def fix asap
+	- Ok no its an issue with proccessing the job file it works fine without anything to do w that
+
+Trying to debug jobfile thing
+	- Let's uncomment line by line until something breaks
+	- OKAY so fclose(fd); singlehandedly caused the segmentation fault! now ??? find out why?
+	- Let's try the while loop without fclose??
+	- No ok that also causes a segmentation fault uh oh
+		- Line by line again??
+		- Is this a waste of time maybe I should just go to office hours lol
+		- It's fine I'll work for a little longer
+		- So the problem is with the file -- maybe it's not being opened correctly?
+		- NO MY JOBFILE NAME WAS MISPELLED YOU'RE JOKING
+		- wow
+		- that's embarrassing
+		- I mean we can add a check for that by making sure the file pointer isn't null before proceeding
+		- Let's do that
+		- Sweet ok that's not ever happening again (for this project...) (hopefully.....) (at least uncaught......)
+
+Now back to trying to get partial functionality running
+	- Line by line uncommenting
+	- Okay yeah so our only problem now is 'int rc = getLine(buffer, 99, fd);
+	- No way is this also mispelled???
+	- No phew
+	- But wait now idk what the issue is still :/
+	- maybe an office hours question tbh... not sure w the docs what's wrong
+
+Maybe try to create nodes now?
+	- Cool I think I created the initial node that listHead points to
+	- Now need to figure out how to add to end of list 
 
 
--- scheduler.c
+++ scheduler.c
@@ -33,35 +33,52 @@ int main(int argc, char *argv[]){
 
     int lineNum = 0;
     
-    while (!feof(fd)){ // While not at the end of the file
-        //int rc = getline(buffer, 99, fd); //need to uncomment eventually, we just tryin to get it to compile
-        int runLength = atoi(buffer);
-    
-        printf("%d", runLength);
+    if(fd != NULL){
+        while (!feof(fd) && lineNum < 100){ // While not at the end of the file //TAKE OUT THE LINENUM THING THIS IS JUST TO KEEP FROM INFINITE LOOP BEFORE WE IMPLEMENT FUNTIONALITY
+            //int rc = getline(*buffer, 99, fd); //this line is causing a segmentation fault :/
+            int runLength = atoi(buffer);
+        
+            printf("%d", runLength);
 
-        // Insert into linked list, runLength and lineNumber as job ID
-        if(listHead == NULL){
-            // Create node to be pointed at it, set listHead to point at it
-        } else{
-            // add to end of list
+            // Insert into linked list, runLength and lineNumber as job ID
+            if(listHead == NULL){
+                // Create node to be pointed at, set listHead to point at it
+
+        //idk if any of this is right lol pls dan help:
+                listHead = malloc(sizeof(struct job));
+
+                struct job *node;
+                node = malloc(sizeof(struct job));
+                    node->id = lineNum; 
+                    node->length = runLength;
+                listHead->next = node;
+
+            } else{
+                // add to end of list
+
+                //uh so theoretically you wanna access prev element, point that to this node.... gotta figure out how to do that
+            }
+            lineNum++; 
         }
-        lineNum++;
-    }
-    fclose(fd);
+        fclose(fd);
 
-    if(strcmp(scheduleType, "FIFO") == 0){ // If given "FIFO"
-        printf("fifo");
-        // DO FIFO STUFF HEREEEE
-    }
+        if(strcmp(scheduleType, "FIFO") == 0){ // If given "FIFO"
+            printf("fifo\n");
+            // DO FIFO STUFF HEREEEE
+        }
 
-    if(strcmp(scheduleType, "SJF") == 0){ // If given "SJF"
-        printf("sjf");
-        // DO SJF STUFF HERE
-    } 
+        if(strcmp(scheduleType, "SJF") == 0){ // If given "SJF"
+            printf("sjf\n");
+            // DO SJF STUFF HERE
+        } 
 
-    if(strcmp(scheduleType, "RR") == 0){ // If given "RR"
-        printf("rr");
-        // DO RR STUFF HERE
+        if(strcmp(scheduleType, "RR") == 0){ // If given "RR"
+            printf("rr\n");
+            // DO RR STUFF HERE
+        }
+        return 0;
+    } else { //if fd is NULL
+        printf("Couldn't find file:(\n");
+        return -1;
     }
-    return 0;
 }
