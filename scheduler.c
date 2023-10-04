cebeaca1e0504d2ab62e7164e9db79d15dda2a4a
Nandita <74115831+ta0202@users.noreply.github.com>
2023-10-04 22:26:30 +0000

    added tests from starter materials on ia
    code is still segmentation faulting tho lol

--- scheduler.c
+++ scheduler.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
     char *scheduleType = argv[1];
     char *jobFile = argv[2];
     char *timeSlice = argv[3];
-
+    listHead = NULL;
     
     
     if(argc != 4){
@@ -34,14 +34,14 @@ int main(int argc, char *argv[]){
     int lineNum = 0;
     
     while (!feof(fd)){ // While not at the end of the file
-        int rc = getline(buffer, 99, fd);
+        //int rc = getline(buffer, 99, fd); //need to uncomment eventually, we just tryin to get it to compile
         int runLength = atoi(buffer);
-
-        printf(runLength);
+    
+        printf("%d", runLength);
 
         // Insert into linked list, runLength and lineNumber as job ID
         if(listHead == NULL){
-            // Create node to be pointed at it
+            // Create node to be pointed at it, set listHead to point at it
         } else{
             // add to end of list
         }
@@ -50,14 +50,17 @@ int main(int argc, char *argv[]){
     fclose(fd);
 
     if(strcmp(scheduleType, "FIFO") == 0){ // If given "FIFO"
+        printf("fifo");
         // DO FIFO STUFF HEREEEE
     }
 
     if(strcmp(scheduleType, "SJF") == 0){ // If given "SJF"
+        printf("sjf");
         // DO SJF STUFF HERE
     } 
 
     if(strcmp(scheduleType, "RR") == 0){ // If given "RR"
+        printf("rr");
         // DO RR STUFF HERE
     }
     return 0;
