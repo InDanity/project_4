7382dd30626bcff093e0d476374c6b948de1aab2
InDanity <101886886+InDanity@users.noreply.github.com>
2023-10-06 14:06:10 +0000

    Fixed the issue with printing out nums (SHUE W)! Specifically, the issue lied with the way we set up the buffer to be an array and not a pointer TO a string. There was also an if statement removed. I assume the rest of the code isn't severely broken for everything to execute fine.

--- scheduler.c
+++ scheduler.c
@@ -30,56 +30,60 @@ int main(int argc, char *argv[]){
 
     // Read input file
     FILE * fd = fopen(jobFile, "r");
-    char buffer[100];
-
+    //char buffer[100];
+    char* buffer = NULL;
+    int readSize = 99;
     int lineNum = 0;
-    
-    if(fd != NULL && lineNum < 100){
-        while (!feof(fd)){ // While not at the end of the file //TAKE OUT THE LINENUM THING THIS IS JUST TO KEEP FROM INFINITE LOOP BEFORE WE IMPLEMENT FUNTIONALITY
-            //int rc = getline(*buffer, 99, fd); //this line is causing a segmentation fault :/
-            int runLength = atoi(buffer);
-        
-            printf("%d", runLength);
 
-            // Insert into linked list, runLength and lineNumber as job ID
-            if(listHead == NULL){
-            // Create node to be pointed at, set listHead to point at it
-
-            //idk if any of this is right lol pls dan help:
-                listHead = malloc(sizeof(struct job));
-
-                struct job *node;
-                node = malloc(sizeof(struct job));
-                node->id = lineNum; 
-                node->length = runLength;
-                listHead->next = node;
-
-            } else{
-                // add to end of list
-
-                //uh so theoretically you wanna access prev element, point that to this node.... gotta figure out how to do that
-            }
-            lineNum++; 
-        }
-        fclose(fd);
-
-        if(strcmp(scheduleType, "FIFO") == 0){ // If given "FIFO"
-            printf("fifo\n");
-            // DO FIFO STUFF HEREEEE
-        }
-
-        if(strcmp(scheduleType, "SJF") == 0){ // If given "SJF"
-            printf("sjf\n");
-            // DO SJF STUFF HERE
-        } 
-
-        if(strcmp(scheduleType, "RR") == 0){ // If given "RR"
-            printf("rr\n");
-            // DO RR STUFF HERE
-        }
-        return 0;
-    } else { //if fd is NULL
-        printf("Couldn't find file:(\n");
-        return -1;
+    if(fd == NULL){
+        printf("ERROR\n");
+        exit(-1);
     }
+    
+    while (getline(&buffer, &readSize, fd) != -1){ // While not at the end of the file //TAKE OUT THE LINENUM THING THIS IS JUST TO KEEP FROM INFINITE LOOP BEFORE WE IMPLEMENT FUNTIONALITY
+        //memset(buffer, 0x00, 100);
+        //int rc = getline(&buffer, &readSize, fd); //this line is causing a segmentation fault :/
+        //printf("%d\n", rc);
+        int runLength = atoi(buffer);
+    
+        //printf("%s\n", buffer);
+        printf("%d\n", runLength);
+
+        // Insert into linked list, runLength and lineNumber as job ID
+        if(listHead == NULL){
+        // Create node to be pointed at, set listHead to point at it
+
+        //idk if any of this is right lol pls dan help:
+            listHead = malloc(sizeof(struct job));
+
+            struct job *node;
+            node = malloc(sizeof(struct job));
+            node->id = lineNum; 
+            node->length = runLength;
+            listHead->next = node;
+
+        } else{
+            // add to end of list
+
+            //uh so theoretically you wanna access prev element, point that to this node.... gotta figure out how to do that
+        }
+        lineNum++; 
+    }
+    fclose(fd);
+
+    if(strcmp(scheduleType, "FIFO") == 0){ // If given "FIFO"
+        printf("fifo\n");
+        // DO FIFO STUFF HEREEEE
+    }
+
+    if(strcmp(scheduleType, "SJF") == 0){ // If given "SJF"
+        printf("sjf\n");
+        // DO SJF STUFF HERE
+    } 
+
+    if(strcmp(scheduleType, "RR") == 0){ // If given "RR"
+        printf("rr\n");
+        // DO RR STUFF HERE
+    }
+    return 0;
 }
