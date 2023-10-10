af3c815a0b58219ad0cd309563efc111a0c2b35e
InDanity <101886886+InDanity@users.noreply.github.com>
2023-10-10 17:35:15 +0000

    Made a BROKEN prototype for a SJF function that takes a list and organizes it based on runlength via bubble sort.

--- scheduler.c
+++ scheduler.c
@@ -8,16 +8,17 @@ struct job{
     int length;
     // Other data
     struct job *next;
+    
     int checked;
  
 };
 
 struct job* listHead;
 
-void jobListPrint(struct job currentJob){ // Will print out the job id and it's runtime, then make the next job do the same thing provided it exist. 
-    printf("Job %d ran for: %d\n", currentJob.id, currentJob.length);
-    if(currentJob.next != NULL){
-        jobListPrint(*currentJob.next);
+void jobListPrint(struct job currJob){ // Will print out the job id and it's runtime, then make the next job do the same thing provided it exist. 
+    printf("Job %d ran for: %d\n", currJob.id, currJob.length);
+    if(currJob.next != NULL){
+        jobListPrint(*currJob.next);
     }
     else{
         printf("That's all, folks!\n");
@@ -25,25 +26,57 @@ void jobListPrint(struct job currentJob){ // Will print out the job id and it's 
     return;
 }
 
-void addNode(struct job* jobList, int jobID, int runLength){ // REMEMBER TO UPDATE AS YOU ADD MORE FIELDS! Copies data of given node to the final node of a list.
-    if(jobList->next == NULL){
+void addNode(struct job* currJob, int jobID, int runLength){ // REMEMBER TO UPDATE AS YOU ADD MORE FIELDS! Copies data of given node to the final node of a list.
+    if(currJob->next == NULL){
         struct job *node;
         node = malloc(sizeof(struct job));
         node->id = jobID; 
         node->length = runLength;
         node->checked = 0;
         
-        jobList->next = node;
+        currJob->next = node;
         return;
     }
     else{
-        addNode(jobList->next, jobID, runLength);
+        addNode(currJob->next, jobID, runLength);
     }
 }
 
-void makeListSJF(struct job* jobList){
+void makeListSJF(struct job* currJob){
+    struct job* tempCurrent = currJob; // used to hold the address of the current node.
+    struct job* tempNewNext;           // used to hold the address of the node 2 nodes away from the current one, provided it EXISTS.
+    struct job* tempNewFirst;          // used to hold the value of the second node (what will be the new first node), in case the first node needs to be switched.
+    while(tempCurrent != NULL){
+        printf("Looking at 3 nodes with lengths %d, %d, and %d.\n", tempCurrent->length, tempCurrent->next->length, tempCurrent->next->next->length);
 
+        if(tempCurrent->length > tempCurrent->next->length){  // If the current node has a smaller run length than the last, SWAPPING TIME!!!
+            printf("Well, %d is greater than %d. Swapping!\n", tempCurrent->length, tempCurrent->next->length);
 
+            if(tempCurrent->next->next != NULL){ // If the node after the next node exists...
+                tempNewNext = tempCurrent->next->next; 
+            }
+            else{ // If the node after the next node does NOT exist...
+               tempNewNext = NULL;
+            }
+
+            if(tempCurrent = currJob->next){ // If this is the first node, AND we were planning on swapping anyhow...
+                tempNewFirst = tempCurrent->next;
+                currJob->next = tempNewFirst; // Make the first node the second node
+            }
+            
+            tempCurrent->next->next = tempCurrent; // Make the next node point to the current node, or "put it behind the current node."
+            printf("Made node w/ length %d point to node w/ length %d.\n", tempCurrent->next->next->length, tempCurrent->length);
+            tempCurrent->next = tempNewNext; // Make the current node point to the previously next node's next node, or "put it behind the next next node."
+
+            printf("Swapped job of length %d with job of length %d.\n", tempCurrent->length, tempCurrent->next->length);
+
+            tempCurrent = currJob; // Start from beginning 
+        }
+        else{ // Otherwise, check the next node
+            tempCurrent = tempCurrent->next;
+        }
+    }
+    return;
 }
 
 
@@ -111,7 +144,8 @@ void main(int argc, char *argv[]){
     }
     else if(strcmp(scheduleType, "SJF") == 0){ // If given "SJF"
         printf("sjf\n");
-        // DO SJF STUFF HERE
+        makeListSJF(listHead);
+
     } 
     else if(strcmp(scheduleType, "RR") == 0){ // If given "RR"
         printf("rr\n");
